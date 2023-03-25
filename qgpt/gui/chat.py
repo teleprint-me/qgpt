03a8b1feb02508e13decf47c5de40dc593c37a4d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-03-24 20:52:22 -0400

    Refactor prototype interface and separate concerns
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- qgpt/gui/chat.py
+++ qgpt/gui/chat.py
@@ -1,3 +1,4 @@
+from assistant import MessageQueue, ResponseThread
 from PySide6.QtCore import Slot
 from PySide6.QtGui import QFont, QTextCharFormat
 from PySide6.QtWidgets import (
@@ -11,15 +12,22 @@ from PySide6.QtWidgets import (
 
 
 class ChatWidget(QWidget):
-    def __init__(self, message_queue, handle_send_message):
+    def __init__(self, main_window):
         super().__init__()
 
-        # Set the message queue and send message handler
-        self.message_queue = message_queue
-        self.handle_send_message = handle_send_message
+        # Create a message queue to manage the chat history
+        self.message_queue = main_window.message_queue
+        # Attach the status bar method to the widget
+        self.update_status_bar = main_window.update_status_bar
+        self.update_status_bar_pending = main_window.update_status_bar_pending
+
+        # Create a response thread
+        self.response_thread = ResponseThread(self.message_queue)
+        self.response_thread.response_generated.connect(self.handle_response)
 
         # Create assistant widgets
-        self.chat_label = QLabel("Chat:")
+        self.chat_label = QLabel("Output:")
+        self.input_label = QLabel("Input:")
         self.chat_history = QPlainTextEdit(self)
         self.chat_history.setReadOnly(True)
         self.input_box = QTextEdit(self)
@@ -27,7 +35,9 @@ class ChatWidget(QWidget):
 
         # Create chat column layout
         chat_column_layout = QVBoxLayout()
+        chat_column_layout.addWidget(self.chat_label)
         chat_column_layout.addWidget(self.chat_history)
+        chat_column_layout.addWidget(self.input_label)
         chat_column_layout.addWidget(self.input_box)
         chat_column_layout.addWidget(self.send_button)
 
@@ -36,6 +46,68 @@ class ChatWidget(QWidget):
         # Connect buttons to corresponding functions
         self.send_button.clicked.connect(self.send_message)
 
+        self.load_chat_history()
+
+    def load_chat_history(self):
+        self.chat_history.clear()
+        for message in self.message_queue.history:
+            role = message["role"]
+            content = message["content"]
+
+            cursor = self.chat_history.textCursor()
+            format_bold = QTextCharFormat()
+            format_bold.setFontWeight(QFont.Bold)
+            format_normal = QTextCharFormat()
+            format_normal.setFontWeight(QFont.Normal)
+
+            cursor.insertText(f"{role.capitalize()}\n", format_bold)
+            cursor.insertText(f"{content}\n\n", format_normal)
+            self.chat_history.setTextCursor(cursor)
+
+    def clear_chat_history(self):
+        self.message_queue.clear()
+        self.load_chat_history()
+
+    def save_chat_history(self):
+        self.message_queue.save()
+
+    @Slot()
     def send_message(self):
-        self.handle_send_message(self.input_box.toPlainText().strip())
+        # Get the user's message from the input box
+        message = self.input_box.toPlainText().strip()
+        # Clear the input box
         self.input_box.clear()
+
+        # Add the user's message to the chat history
+        cursor = self.chat_history.textCursor()
+        format_bold = QTextCharFormat()
+        format_bold.setFontWeight(QFont.Bold)
+        format_normal = QTextCharFormat()
+        format_normal.setFontWeight(QFont.Normal)
+
+        cursor.insertText("User\n", format_bold)
+        cursor.insertText(f"{message}\n\n", format_normal)
+        self.chat_history.setTextCursor(cursor)
+
+        # Add user message to message queue
+        self.message_queue.add_message("user", message)
+        # Update the status bar to show "message pending"
+        self.update_status_bar_pending()
+        # Start the response thread
+        self.response_thread.start()
+
+    @Slot(str)
+    def handle_response(self, response):
+        # Add the assistant's response to the chat history
+        cursor = self.chat_history.textCursor()
+        format_bold = QTextCharFormat()
+        format_bold.setFontWeight(QFont.Bold)
+        format_normal = QTextCharFormat()
+        format_normal.setFontWeight(QFont.Normal)
+
+        cursor.insertText("Assistant\n", format_bold)
+        cursor.insertText(f"{response}\n\n", format_normal)
+        self.chat_history.setTextCursor(cursor)
+
+        # Update the status bar to show "message complete"
+        self.update_status_bar()
--- qgpt/gui/mainwindow.py
+++ qgpt/gui/mainwindow.py
@@ -1,10 +1,5 @@
-from PySide6.QtWidgets import (
-    QApplication,
-    QHBoxLayout,
-    QMainWindow,
-    QStatusBar,
-    QWidget,
-)
+from assistant.queue import MessageQueue
+from PySide6.QtWidgets import QHBoxLayout, QMainWindow, QStatusBar, QWidget
 
 from .chat import ChatWidget
 from .editor import EditorWidget
@@ -12,21 +7,21 @@ from .menubar import MenuBar
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, message_queue):
+    def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Q-GPT")
         self.setGeometry(200, 200, 500, 500)
 
         # Set the message queue
-        self.message_queue = message_queue
+        self.message_queue = MessageQueue()
 
         # Create menu bar
         menu_bar = MenuBar()
         self.setMenuBar(menu_bar)
 
         # Create ChatWidget and EditorWidget
-        self.chat_widget = ChatWidget(self.message_queue, self.send_message)
+        self.chat_widget = ChatWidget(self)
         self.editor_widget = EditorWidget()
 
         # Main layout
@@ -43,8 +38,26 @@ class MainWindow(QMainWindow):
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
 
-    def send_message(self, message):
-        # Process the message and update chat history and status bar
-        # This method should be implemented based on your current Q_GPT.send_message() logic
+        self.update_status_bar()
 
-        pass
+    def update_status_bar(self):
+        self.statusBar.clearMessage()
+        count = str(self.message_queue.token_count)
+        assistant = self.message_queue.assistant.capitalize()
+        max = self.message_queue.max_tokens
+        self.statusBar.showMessage(
+            f"Role: {assistant} | Max Tokens: {max} | Total Tokens: {count}"
+        )
+
+    def update_status_bar_pending(self):
+        self.statusBar.clearMessage()
+        assistant = self.message_queue.assistant.capitalize()
+        self.statusBar.showMessage(
+            f"{assistant} is processing your request..."
+        )
+
+    def closeEvent(self, event):
+        # Save the queue state before exiting
+        self.message_queue.save()
+        # Exit program
+        event.accept()
